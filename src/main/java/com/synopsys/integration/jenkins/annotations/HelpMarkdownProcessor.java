36d76910b7b890a272c469f3211df65e8710c4a8
Richard Otte <rotte@blackducksoftware.com>
2020-07-08 17:06:28 -0400

    feat: Add Java 11 support

--- src/main/java/com/synopsys/integration/jenkins/annotations/HelpMarkdownProcessor.java
+++ src/main/java/com/synopsys/integration/jenkins/annotations/HelpMarkdownProcessor.java
@@ -45,14 +45,14 @@ import javax.tools.Diagnostic;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class HelpMarkdownProcessor extends AbstractProcessor {
     private Filer filer;
     private Messager messager;
     private Elements elementUtils;
 
     @Override
-    public synchronized void init(final ProcessingEnvironment processingEnv) {
+    public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         filer = processingEnv.getFiler();
         messager = processingEnv.getMessager();
@@ -60,25 +60,25 @@ public class HelpMarkdownProcessor extends AbstractProcessor {
     }
 
     @Override
-    public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         try {
             if (roundEnv.processingOver()) {
                 return true;
             }
 
-            final HelpHtmlGenerator helpHtmlGenerator = new HelpHtmlGenerator(filer);
+            HelpHtmlGenerator helpHtmlGenerator = new HelpHtmlGenerator(filer);
 
-            final Set<TypeElement> typeElements = getAnnotatedClassesAsTypeElements(roundEnv.getRootElements(), annotations);
-            for (final TypeElement typeElement : typeElements) {
-                final String packageString = elementUtils.getPackageOf(typeElement).getQualifiedName().toString();
-                final String typeName = typeElement.getSimpleName().toString();
-                final String resourcePackage = packageString + "." + typeName;
+            Set<TypeElement> typeElements = getAnnotatedClassesAsTypeElements(roundEnv.getRootElements(), annotations);
+            for (TypeElement typeElement : typeElements) {
+                String packageString = elementUtils.getPackageOf(typeElement).getQualifiedName().toString();
+                String typeName = typeElement.getSimpleName().toString();
+                String resourcePackage = packageString + "." + typeName;
 
-                for (final VariableElement variableElement : ElementFilter.fieldsIn(typeElement.getEnclosedElements())) {
+                for (VariableElement variableElement : ElementFilter.fieldsIn(typeElement.getEnclosedElements())) {
                     helpHtmlGenerator.generateHelpHtmlFromAnnotation(resourcePackage, variableElement);
                 }
             }
-        } catch (final Exception e) {
+        } catch (Exception e) {
             messager.printMessage(Diagnostic.Kind.ERROR, ExceptionUtils.getStackTrace(e));
         }
         return true;
@@ -89,7 +89,7 @@ public class HelpMarkdownProcessor extends AbstractProcessor {
         return Stream.of(HelpMarkdown.class.getCanonicalName()).collect(Collectors.toSet());
     }
 
-    private Set<TypeElement> getAnnotatedClassesAsTypeElements(final Set<? extends Element> elements, final Set<? extends Element> supportedAnnotations) {
+    private Set<TypeElement> getAnnotatedClassesAsTypeElements(Set<? extends Element> elements, Set<? extends Element> supportedAnnotations) {
         return elements.stream()
                    .filter(TypeElement.class::isInstance)
                    .map(TypeElement.class::cast)
@@ -97,7 +97,7 @@ public class HelpMarkdownProcessor extends AbstractProcessor {
                    .collect(Collectors.toSet());
     }
 
-    private boolean elementContainsSupportedAnnotation(final Element element, final Set<? extends Element> supportedAnnotations) {
+    private boolean elementContainsSupportedAnnotation(Element element, Set<? extends Element> supportedAnnotations) {
         return element.getEnclosedElements().stream()
                    .map(Element::getAnnotationMirrors)
                    .flatMap(List::stream)
---
+++ src/test/java/com/synopsys/integration/jenkins/annotations/HelpHtmlGeneratorTest.java
@@ -0,0 +1,66 @@
+package com.synopsys.integration.jenkins.annotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.processing.Filer;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.VariableElement;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class HelpHtmlGeneratorTest {
+    @Test
+    public void testGenerateHelpHtmlFromAnnotation() throws IOException {
+        String variableName = "variableName";
+        String annotationContent = "* foo\r\n* bar";
+        String resourcePackage = "com.synopsys.integration.jenkins.Example";
+
+        String expectedFileName = "help-" + variableName + ".html";
+        List<String> expectedFileContents = Arrays.asList("<div>", "<ul>", "<li>foo</li>", "<li>bar</li>", "</ul>", "</div>", "");
+
+        Path testHelpHtmlOutput = Files.createTempFile("test-" + expectedFileName, null);
+        testHelpHtmlOutput.toFile().deleteOnExit();
+
+        // Set up mocks
+        FileObject mockFileObject = Mockito.mock(FileObject.class);
+        Mockito.when(mockFileObject.openWriter())
+            .thenReturn(Files.newBufferedWriter(testHelpHtmlOutput));
+        Filer mockFiler = Mockito.mock(Filer.class);
+        Mockito.when(mockFiler.createResource(StandardLocation.CLASS_OUTPUT, resourcePackage, expectedFileName))
+            .thenReturn(mockFileObject);
+        Name mockedName = Mockito.mock(Name.class);
+        Mockito.when(mockedName.toString())
+            .thenReturn(variableName);
+        HelpMarkdown mockedHelpMarkdown = Mockito.mock(HelpMarkdown.class);
+        Mockito.when(mockedHelpMarkdown.value())
+            .thenReturn(annotationContent);
+        VariableElement mockedVariableElement = Mockito.mock(VariableElement.class);
+        Mockito.when(mockedVariableElement.getAnnotation(HelpMarkdown.class))
+            .thenReturn(mockedHelpMarkdown);
+        Mockito.when(mockedVariableElement.getSimpleName())
+            .thenReturn(mockedName);
+
+        // Test
+        HelpHtmlGenerator helpHtmlGenerator = new HelpHtmlGenerator(mockFiler);
+        try {
+            helpHtmlGenerator.generateHelpHtmlFromAnnotation(resourcePackage, mockedVariableElement);
+        } catch (IOException e) {
+            fail("Unexpected IOException thrown-- test should be intercepting file creation so this shouldn't be happening, please review the test code.", e);
+        }
+
+        List<String> actualFileContents = Files.readAllLines(testHelpHtmlOutput);
+        for (int i = 0; i < actualFileContents.size(); i++) {
+            assertEquals(expectedFileContents.get(i), actualFileContents.get(i));
+        }
+    }
+}
